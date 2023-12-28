de86c51bbb4d5093ddd178959c1346946a00a1f7
srb <saurabh.borse@alumni.fh-aachen.de>
2023-12-28 13:13:27 +0100

    code commenting and clearing the unnecessary files

--- robotino_description/launch/robotino_description.launch.py
+++ robotino_description/launch/robotino_description.launch.py
@@ -1,7 +1,28 @@
+# Author: Saurabh Borse(saurabh.borse@alumni.fh-aachen.de)
+
+#  MIT License
+#  Copyright (c) 2022 Saurabh Borse
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
 #!/usr/bin/env python3
 
 import os
-
 import launch
 import launch_ros
 from ament_index_python.packages import get_package_share_directory
@@ -17,24 +38,30 @@ def generate_launch_description():
     default_model_path = os.path.join(pkg_share, "urdf/robots/robotino3_description.urdf")
     default_rviz2_path = os.path.join(pkg_share, "rviz/robotino3_description.rviz")
  
+    # Initialize robot state publisher 
     robot_state_publisher_node = launch_ros.actions.Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": Command(["xacro ", LaunchConfiguration("model")])}],
     )
 
+    # Initialize joint state broadcaster
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
         name="joint_state_publisher",
         condition=launch.conditions.UnlessCondition(LaunchConfiguration("gui")),
     )
+    
+    # Initialize joint state publisher gui
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
         name="joint_state_publisher_gui",
         condition=launch.conditions.IfCondition(LaunchConfiguration("gui")),
     )
+    
+    # Initialize rviz2
     rviz_node = launch_ros.actions.Node(
         package="rviz2",
         executable="rviz2",
@@ -43,9 +70,9 @@ def generate_launch_description():
         arguments=["-d" + default_rviz2_path],
     )
 
-
     return LaunchDescription([
-            
+        
+        # Declare launch arguments
         launch.actions.DeclareLaunchArgument(
             name="gui", default_value="True", description="Flag to enable joint_state_publisher_gui"
         ),
@@ -55,6 +82,8 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             name="rvizconfig", default_value=default_rviz2_path, description="Absolute path to rviz config file"
         ),
+        
+        # Launch nodes
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
